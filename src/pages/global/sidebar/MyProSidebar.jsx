8b110661c0cd5abb307d2bee7457dcf436157bee
monarxxjain <jainmonark1000@gmail.com>
2023-10-06 02:03:51 +0530

    Sidebar Made

---
+++ src/pages/global/sidebar/MyProSidebar.jsx
@@ -0,0 +1,251 @@
+// docs https://github.com/azouaoui-med/react-pro-sidebar
+import { useEffect, useState } from "react";
+import { Menu, Sidebar, MenuItem } from "react-pro-sidebar";
+import { useProSidebar } from "react-pro-sidebar";
+
+import { useSidebarContext } from "./sidebarContext";
+
+import { Link } from "react-router-dom";
+import { tokens } from "../../../theme";
+import { useTheme, Box, Typography, IconButton } from "@mui/material";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import SmartDisplayOutlinedIcon from '@mui/icons-material/SmartDisplayOutlined';
+import MusicVideoIcon from '@mui/icons-material/MusicVideo';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import RestoreOutlinedIcon from '@mui/icons-material/RestoreOutlined';
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import SwitchRightOutlinedIcon from "@mui/icons-material/SwitchRightOutlined";
+import SwitchLeftOutlinedIcon from "@mui/icons-material/SwitchLeftOutlined";
+import MacButtonsIcon from "../../../assets/sidebar/MacButtonsIcon";
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode)
+
+  return (
+    <MenuItem
+      active={selected === title}
+      style={{ color: colors.grey[100], borderBottom: "1px solid transparent",
+        borderImage: selected === title ? "linear-gradient(to right, transparent, rgba(196, 39, 251, 0.3), transparent)" : "linear-gradient(to right, transparent, rgba(255,255,255,0.2), transparent)",
+        borderImageSlice: 1,
+        borderTop: selected === title ? "1px solid transparent" : "none",
+        position: "relative"
+        }}
+      onClick={() => setSelected(title)}
+      icon={icon}
+      routerLink={<Link to={to} />}
+    >
+        {selected === title && <svg style={{position:"absolute", left: "20px", top: "7px"}} xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34" fill="none">
+          <path id="Polygon 7" d="M6 6C12.0751 -0.0751297 21.9249 -0.0751297 28 6V6C34.0751 12.0751 34.0751 21.9249 28 28V28C21.9249 34.0751 12.0751 34.0751 6 28V28C-0.0751297 21.9249 -0.0751297 12.0751 6 6V6Z" stroke="url(#paint0_linear_2116_14497)"/>
+          <defs>
+          <linearGradient id="paint0_linear_2116_14497" x1="2.15" y1="0.5" x2="34.05" y2="29.1" gradientUnits="userSpaceOnUse">
+          <stop stop-color="#A73EE7"/>
+          <stop offset="1" stop-color="#00EBFF"/>
+          </linearGradient>
+          </defs>
+        </svg>}
+        <Typography style={{fontSize: "17px", fontWeight: "590"}}>{title}</Typography>
+    </MenuItem>
+  );
+};
+
+const MyProSidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [selected, setSelected] = useState("Dashboard");
+  const { sidebarRTL, setSidebarRTL, sidebarImage } = useSidebarContext();
+  const { collapseSidebar, toggleSidebar, collapsed, broken } = useProSidebar();
+  const pages = [
+    {
+      title: "Dashboard",
+      to: "/",
+      icon: <HomeOutlinedIcon />
+    },
+    {
+      title: "Personal",
+      to: "/",
+      icon: <SmartDisplayOutlinedIcon />
+    },
+    {
+      title: "inventory",
+      to: "/",
+      icon: <SubscriptionsOutlinedIcon />
+    },
+    {
+      title: "Sales",
+      to: "/",
+      icon: <MusicVideoIcon />
+    },
+    {
+      title: "Orders",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Reports",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Administration",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Yeasti Ai",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Customers",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Suppliers",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Logistics",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Marketing",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Financials",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+    {
+      title: "Compliance",
+      to: "/",
+      icon: <SlideshowIcon />
+    },
+
+  ]
+  return (
+    <Box
+      sx={[{
+        position: "sticky",
+        display: "flex",
+        height: "100vh",
+        top: 0,
+        bottom: 0,
+        zIndex: 10000,
+        "& .sidebar": {
+          border: "none",
+          background: "linear-gradient(168deg, rgba(46, 51, 90, 0.50) 1.62%, rgba(28, 27, 51, 0.50) 95.72%)",
+          boxShadow: "inset -50px -10px 60px -40px rgba(59, 38, 123, 0.70)"
+        },
+        "& .menu-icon": {
+          backgroundColor: "transparent !important",
+        },
+        "& .menu-item": {
+          // padding: "5px 35px 5px 20px !important",
+          backgroundColor: "transparent !important",
+          // backgroundImage: "linear-gradient(to right, transparent, #f0f0f0, transparent)",
+          // backgroundOrigin: "border-box",
+          // backgroundClip: "content-box"
+        },
+        "& .menu-anchor": {
+          color: "inherit !important",
+          backgroundColor: "transparent !important",
+        },
+        "& .menu-item:hover": {
+          backgroundColor: "transparent !important",
+          transform: "scale(1.1)",
+          transition: "all 0.4"
+        },
+        "& .menu-item.active": {
+          background: "linear-gradient(to bottom, rgba(196, 39, 251, 0.3) 50%, transparent) !important",
+        },
+      }, collapsed ? {
+        "& .sidebar": {
+          // border: "1px solid white",
+          borderRadius: "15px !important",
+          margin: "5px"
+        },
+      } : {}]}
+    >
+      <Sidebar
+        breakPoint="md"
+        rtl={sidebarRTL}
+        backgroundColor={"background: linear-gradient(168deg, rgba(46, 51, 90, 0.50) 1.62%, rgba(28, 27, 51, 0.50) 95.72%)"}
+        image={sidebarImage}
+        
+      >
+        <Menu iconshape="square">
+          <MenuItem
+            icon={
+              <MacButtonsIcon/>
+            }
+            style={!collapsed ? {marginLeft: "20px", marginTop: "10px"} : {marginLeft: "2px", marginTop: "10px"}}
+          >
+          </MenuItem>
+          <MenuItem
+            style={{
+              margin: "10px 0 0 0",
+              color: colors.grey[100],
+            }}
+          >
+            {collapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="7px"
+              >
+                <MenuOutlinedIcon onClick={() => collapseSidebar()} />
+              </Box>
+            )}
+            {!collapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                ml="15px"
+              >
+                <MenuOutlinedIcon onClick={() => collapseSidebar()} />
+                <Typography variant="h2" color={colors.grey[100]} style={{margin: "0 20px 0 0"}}>
+                  logoHere
+                </Typography>
+
+              </Box>
+            )}
+          </MenuItem>
+          
+          <Box marginX={!collapsed ? "16px" : undefined}>
+            {pages.map((page, id)=>{
+              return <Item 
+                key={id}
+                title={page.title}
+                to={page.to}
+                icon={page.icon}
+                selected={selected}
+                setSelected={setSelected}
+              />
+            })}
+          </Box>
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
+};
+
+export default MyProSidebar;
